a0ad7aa144d055f2e8bdbaab8cb084e1b5462d03
NadiaSchmitz <nadiiaschmitz@gmail.com>
2022-01-12 10:43:33 +0100

    12012022

--- src/Noten.java
+++ src/Noten.java
@@ -10,34 +10,33 @@ import java.text.DecimalFormat;
 
 public class Noten extends JFrame {
 	
-	int i, check;
-	double[] noten = new double[10];
+	int i, anzahl;
+	double[] noten = new double[20];
 	double summe, dunrschnitsnote;
 	JLabel header, linie;
-	JTextField note_1, note_2, note_3;
 	JButton button_rechnen;
-	JLabel[] labels = new JLabel[10];
-	JTextField[] text_fields = new JTextField[10]; 
+	JLabel[] labels = new JLabel[20];
+	JTextField[] text_fields = new JTextField[20]; 
 	
 	DecimalFormat df = new DecimalFormat("###.#");
 	
 	public Noten() {
 		
 		super("Durschnitsnote");
-		super.setBounds(200, 200, 300, 600);
+		super.setBounds(200, 200, 350, 600);
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		Container container = super.getContentPane();
-		container.setLayout(new GridLayout(20, 1, 1, 5));
+		container.setLayout(new GridLayout(22, 1, 1, 5));
 		getRootPane().setBorder(BorderFactory.createEmptyBorder(10, 20, 40, 20));
 		
 		header = new JLabel("Geben Sie Ihre Noten ein:");
 		container.add(header);
-		linie = new JLabel();
+		linie = new JLabel(" ");
 		container.add(linie);
 		
 		for (i = 0; i < labels.length; i++) {
-			labels[i] = new JLabel("Lernfeld " + (i + 1));
+			labels[i] = new JLabel("Lernfeld " + (i + 1) + "     1-5");
 			container.add(labels[i]);
 			text_fields[i] = new JTextField();
 			container.add(text_fields[i]);
@@ -51,24 +50,37 @@ public class Noten extends JFrame {
 	}
 	
 	class SendEvent implements ActionListener {
+		
 		DecimalFormat df = new DecimalFormat("###.#");
 		
 		public void actionPerformed(ActionEvent e) {
 			
-			for (i = 0; i < 10; i++) {
-				noten[i] = Integer.parseInt(text_fields[i].getText());
+			summe = 0;
+			
+			for (i = 0; i < 20; i++) {
+				
+				if (text_fields[i].getText().equals("")) {
+					noten[i] = 0;
+				} else {
+					noten[i] = Integer.parseInt(text_fields[i].getText());
+				}
 				System.out.println(noten[i]);
 				summe = summe + noten[i];
+				
+				if (noten[i] > 0) {
+					anzahl = anzahl + 1;
+				}
 			}
 			
-			dunrschnitsnote = summe / noten.length;
+			dunrschnitsnote = summe / anzahl;
 			
 			System.out.println("Summe: " + summe);
-			System.out.println("Anzahl: " + noten.length);
+			System.out.println("Anzahl: " + anzahl);
 			System.out.println("Mittel: " + df.format(dunrschnitsnote));
 			
 			JOptionPane.showMessageDialog(null, "" + df.format(dunrschnitsnote), "Ihre Dunrschnitsnote", JOptionPane.PLAIN_MESSAGE);
 			summe = 0;
+			anzahl = 0;
 
 		}
 		
